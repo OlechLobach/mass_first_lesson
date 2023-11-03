fbff22c1b0240e4436e70f25b8dc99e275b7cd7f
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-03 19:47:11 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
 
 int main() {
+    const int numMonths = 6;
+    double profits[numMonths];
 
+
+    for (int i = 0; i < numMonths; i++) {
+        std::cout << "Enter the profit for month " << i + 1 << ": ";
+        std::cin >> profits[i];
+    }
+
+    double totalProfit = 0.0;
+
+    for (int i = 0; i < numMonths; i++) {
+        totalProfit += profits[i];
+    }
+
+    double averageProfit = totalProfit / numMonths;
+
+    std::cout << "Total profit for 6 months: " << totalProfit << std::endl;
+    std::cout << "Average profit for 6 months: " << averageProfit << std::endl;
 	return 0;
 }
