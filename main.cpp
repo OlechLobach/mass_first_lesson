2b1591557970c5019a5ccfd43c21aca83e82aa9c
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-03 19:55:22 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include<iostream>
 
 int main() {
+    const int numSides = 5;
+    double sides[numSides]; 
+
+    std::cout << "Enter the lengths of the sides of the pentagon:" << std::endl;
+    for (int i = 0; i < numSides; i++) {
+        std::cout << "Side " << i + 1 << ": ";
+        std::cin >> sides[i];
+    }
+
+    double perimeter = 0.0;
+
+    for (int i = 0; i < numSides; i++) {
+        perimeter += sides[i];
+    }
+
+    std::cout << "Perimeter of the pentagon: " << perimeter << std::endl;
 
 	return 0;
 }
