5ef04fc3546e7f9deebcc97873831ca5949a3bf1
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-03 20:11:58 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
-#include<iostream>
+#include <iostream>
+#include <ctime>
 
 int main() {
+    const int arraySize = 10; 
+    int arr[arraySize]; 
 
-	return 0;
+ 
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+
+    for (int i = 0; i < arraySize; i++) {
+        arr[i] = std::rand() % 100; 
+    }
+
+  
+    std::cout << "Random array: ";
+    for (int i = 0; i < arraySize; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    int minElement = arr[0]; 
+    int maxElement = arr[0]; 
+
+    
+    for (int i = 1; i < arraySize; i++) {
+        if (arr[i] < minElement) {
+            minElement = arr[i];
+        }
+        if (arr[i] > maxElement) {
+            maxElement = arr[i];
+        }
+    }
+
+   
+    std::cout << "Minimum element: " << minElement << std::endl;
+    std::cout << "Maximum element: " << maxElement << std::endl;
+
+    return 0;
 }
