bbd55eb9aaa12598674ff35fd52bad79bc8c0022
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-03 19:51:01 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 
 int main() {
+    const int arraySize = 5; 
+    int arr[arraySize]; 
 
+    
+    std::cout << "Enter " << arraySize << " integer numbers:" << std::endl;
+    for (int i = 0; i < arraySize; i++) {
+        std::cin >> arr[i];
+    }
+
+
+    std::cout << "Array in reverse order: ";
+    for (int i = arraySize - 1; i >= 0; i--) {
+        std::cout << arr[i] << " ";
+    }
+
+    std::cout << std::endl;
 	return 0;
 }
